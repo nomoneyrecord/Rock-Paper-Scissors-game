e22f11a880b201f79e1448fab20d02554b9b9ed6
Gary H <3rdeye.ghj@gmail.com>
2023-01-16 12:55:57 -0800

    Added event listners
    Started building the logic for the game play

---
+++ script.js
@@ -0,0 +1,18 @@
+let userWins = 0;
+let computerWins = 0;
+
+const rockButton = document.getElementById("rock");
+rockButton.addEventListener("click", function () {
+});
+
+const paperButton = document.getElementById("paper");
+paperButton.addEventListener("click", function () {
+});
+
+const scissorButton = document.getElementById("scissor");
+scissorButton.addEventListener("click", function () {
+});
+
+function playGame(userChoice, computerChoice) {
+  if userChoice = 
+}
