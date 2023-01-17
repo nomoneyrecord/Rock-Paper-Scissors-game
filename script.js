404743effebbc0287dcd06ea5e96d48e272d643a
Gary H <3rdeye.ghj@gmail.com>
2023-01-16 16:34:57 -0800

    Tried a couple different logics
    Attempted a couple different approaches to the game logic but still trying to find functions

--- script.js
+++ script.js
@@ -1,18 +1,15 @@
-let userWins = 0;
-let computerWins = 0;
-
 const rockButton = document.getElementById("rock");
-rockButton.addEventListener("click", function () {
+rockButton.addEventListener("click", function() {
+
 });
 
 const paperButton = document.getElementById("paper");
-paperButton.addEventListener("click", function () {
+paperButton.addEventListener("click", function() {
+
 });
 
 const scissorButton = document.getElementById("scissor");
-scissorButton.addEventListener("click", function () {
+scissorButton.addEventListener("click", function() {
+
 });
 
-function playGame(userChoice, computerChoice) {
-  if userChoice = 
-}
