fa7469fc9dc66a3dabb304c2df23262ffdbe9f30
Gary H <3rdeye.ghj@gmail.com>
2023-01-18 12:11:23 -0800

    Made changes to event listeners
    tried a different approach with addEventListener and trying if/else arguments

--- script.js
+++ script.js
@@ -1,15 +1,26 @@
-const rockButton = document.getElementById("rock");
-rockButton.addEventListener("click", function() {
+const game = () => {
+	let playerScore = 0;
+	let computerScore = 0;
 
-});
+	const playGame = () => {
+		const rock = document.getElementById('rock');
+		const paper = document.getElementById('paper');
+		const scissor = document.getElementById('scissor');
+		const playerOptions = [rock,paper,scissor];
+		const computerOptions = ['rock','paper','scissor']
+		
+		playerOptions.forEach(option => {
+			option.addEventListener('click',function(){
+				const choiceNumber = Math.floor(Math.random()*3);
+				const computerChoice = computerOptions[choiceNumber];
+				winner(this.innerText,computerChoice)
+			})
+		})
+  }
+	
+	playGame();
+	
+}
 
-const paperButton = document.getElementById("paper");
-paperButton.addEventListener("click", function() {
-
-});
-
-const scissorButton = document.getElementById("scissor");
-scissorButton.addEventListener("click", function() {
-
-});
+game();
 
