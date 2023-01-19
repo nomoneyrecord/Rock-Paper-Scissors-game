876a8c2e4abf2b28124c302b6da53cd0ae4ff03c
Gary H <3rdeye.ghj@gmail.com>
2023-01-19 11:47:36 -0800

    Changed approach and started fresh
    Was challenged with getting my logic to work so started fresh for the practice and observing different techniques to generate similar results

--- script.js
+++ script.js
@@ -1,26 +1,18 @@
-const game = () => {
-	let playerScore = 0;
-	let computerScore = 0;
-
-	const playGame = () => {
-		const rock = document.getElementById('rock');
-		const paper = document.getElementById('paper');
-		const scissor = document.getElementById('scissor');
-		const playerOptions = [rock,paper,scissor];
-		const computerOptions = ['rock','paper','scissor']
-		
-		playerOptions.forEach(option => {
-			option.addEventListener('click',function(){
-				const choiceNumber = Math.floor(Math.random()*3);
-				const computerChoice = computerOptions[choiceNumber];
-				winner(this.innerText,computerChoice)
-			})
-		})
-  }
-	
-	playGame();
-	
-}
-
-game();
-
+//const game = () => {
+//	let playerScore = 0;
+//	let computerScore = 0;
+//
+//	const playGame = () => {
+//		const rock = document.getElementById('rock');
+//		const paper = document.getElementById('paper');
+//		const scissor = document.getElementById('scissor');
+//		const playerOptions = [rock,paper,scissor];
+//		const computerOptions = ['rock','paper','scissor']
+//		
+//		playerOptions.forEach(option => {
+//			option.addEventListener('click',function(){
+//				const choiceNumber = Math.floor(Math.random()*3);
+//				const computerChoice = computerOptions[choiceNumber];
+//				winner(this.innerText,computerChoice)
+//			})
+//		})
