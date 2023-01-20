45b22ad4e5b04e20d99e1f32150ae31a2d955a5d
Gary H <3rdeye.ghj@gmail.com>
2023-01-20 12:16:35 -0800

    Everything is wired and building the game logic

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const choices = document.querySelectoryAll(".choice");
+const choices = document.querySelectorAll(".choice");
 const score = document.getElementById("score");
 const result = document.getElementById("result");
 const restart = document.getElementById("restart");
@@ -10,6 +10,59 @@ const scoreboard = {
 
 //Play game
 function play(e){
-  console.log(e.target.id)
+  restart.style.display = "inline-block";
+  const playerChoice = e.target.id;
+  const computerChoice = getComputerChoice();
+  const winner = getWinner(playerChoice, computerChoice);
+
+  console.log(playerChoice, computerChoice, winner);
 }
+ //Get computers choice
+function getComputerChoice() {
+  const rand = Math.random();
+  if(rand < 0.34) {
+    return "rock";
+  } else if (rand <= 0.67) {
+    return "paper";
+  } else {
+    return "scissor";
+  }
+}
+
+// Get game winner
+function getWinner(p, c) {
+  if(p === c) {
+    return "draw";
+  } else if(p === "rock") {
+    if(c === "paper") {
+      return "computer";
+    } else {
+      return "player";
+    }
+  } else if(p === "paper") {
+    if(c === "scissor") {
+      return "computer";
+    } else {
+      return "player";
+    }
+  } else if(p === "scissor") {
+    if(c === "rock") {
+      return "computer";
+    } else {
+      return "player";
+    }
+  }
+}
+
+function showWinner(winner, computerChoice) {
+  if(winner === "player") {
+    scoreboard.player++;
+    result.innerTEXT = `
+      <h1 class="text-win">You Win</h1>
+      <`
+  }
+}
+
+//Event listeners
+choices.forEach(choice => choice.addEventListener("click", play));
 
